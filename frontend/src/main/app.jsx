ee35df8bb369bebe9beb611f253685651762f26f
Camila <camilanobre170@gmail.com>
2025-03-25 15:16:21 -0300

    Components
    Criação de components para Dashboard

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,14 +4,15 @@ import React from "react";
 import Header from "../common/template/header"
 import Sidebar from "../common/template/sidebar"
 import Footer from '../common/template/footer';
+import Routes from './routes/routes';
 
 export default props => (
     <div className="wrapper">
         <Header />
         <Sidebar />
         <div className='content-wrapper'>
-            <h1>Conteúdo</h1>
+            <Routes />
         </div>
-        <Footer/>
+       <Footer/>
     </div>
 )
