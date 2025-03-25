9e2945c727084281a3ed4a554d46465b474a1d18
Camila <camilanobre170@gmail.com>
2025-03-25 16:24:37 -0300

    backend
    atualização de backend

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect(
-    'mongodb://localhost/mymoney', 
-    options
-)
+module.exports = mongoose.connect('mongodb://localhost/mymoney')
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = 
